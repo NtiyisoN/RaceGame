807ee0e77792e3499f67599bdb5dbce050b37ce8
palexa <pytsik@mail.ru>
2018-11-29 22:52:06 +0300

    Creating modules

---
+++ Web/Frontend/RG/src/app/modules/game/game.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GameWindowComponent } from './game-window/game-window.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [GameWindowComponent]
+})
+export class GameModule { }
