a1559857b79b703bf2d87d22fd03bb7fdb0d6e35
palexa <pytsik@mail.ru>
2019-01-04 00:01:20 +0300

    server updating

--- Web/Backend/server-example/server/server.js
+++ Web/Backend/server-example/server/server.js
@@ -1,30 +1,9 @@
-var loopback = require('loopback');
-var boot = require('loopback-boot');
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-var app = module.exports = loopback();
-// parse application/json
-app.use(bodyParser.json());
+app.get('/', (req, res) => res.send('Hello World!')
+);
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
-
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`)
+);
